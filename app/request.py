3c3259ff5bf268c794d2ae9fe68804456ab9b736
Иван Максимов <ivan@MacBook-Pro-Ivan.local>
2023-05-02 20:06:59 +0300

    add request

--- app/request.py
+++ app/request.py
@@ -1,18 +1,23 @@
 import requests
 
-url = 'http://127.0.0.1:8000/v1/predict/'
-data = {"data": [{ "sepal_length": 5.1,
-                        "sepal_width": 3.5,
-                        "petal_length": 1.4,
-                        "petal_width": 0.2
-                        },
-                        { "sepal_length": 6.2,
-                         "sepal_width": 3.4,
-                         "petal_length": 5.4,
-                         "petal_width": 2.3
-                         }, 
-                         ]}
+URL = "http://127.0.0.1:8000/v1/predict/"
+data = {
+    "data": [
+        {
+            "sepal_length": 5.1,
+            "sepal_width": 3.5,
+            "petal_length": 1.4,
+            "petal_width": 0.2,
+        },
+        {
+            "sepal_length": 6.2,
+            "sepal_width": 3.4,
+            "petal_length": 5.4,
+            "petal_width": 2.3,
+        },
+    ]
+}
 
-x = requests.post(url, json=data)
+x = requests.post(URL, json=data, timeout=1)
 print(x.status_code)
 print(x.text)
